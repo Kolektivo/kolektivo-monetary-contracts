ef546592421c4d113a89d7a7d399f35246710e05
pascal <pascal@merkleplant.xyz>
2022-04-05 12:17:59 +0200

    Reserve: Adds price oracles, owner funcs and sceleton trade funcs

--- src/Reserve.sol
+++ src/Reserve.sol
@@ -8,8 +8,12 @@ import {Ownable} from "solrocket/Ownable.sol";
 import {Whitelisted} from "solrocket/Whitelisted.sol";
 
 import {Treasury} from "./Treasury.sol";
-
 import {KOL} from "./KOL.sol";
+
+interface IOracle {
+    // Note that the price is returned with 18 decimal precision.
+    function getData() external returns (uint, bool);
+}
 
 contract Reserve is Ownable, Whitelisted {
     using SafeTransferLib for ERC20;
@@ -22,15 +26,30 @@ contract Reserve is Ownable, Whitelisted {
     /// @param minBackingInBPS The min amount of backing allowed, in bps.
     error SupplyExceedsReserveLimit(uint backingInBPS, uint minBackingInBPS);
 
+    /// @notice Functionality is limited due to stale price delivered by oracle.
+    /// @param asset The address of the asset.
+    /// @param oracle The address of the asset's oracle.
+    error StalePriceDeliveredByOracle(address asset, address oracle);
+
+    /// @notice Event emitted when an asset's oracle is updated.
+    /// @param asset The address of the asset.
+    /// @param oldOracle The address of the asset's old oracle.
+    /// @param newOracle The address of the asset's new oracle.
+    event AssetOracleUpdated(address indexed asset,
+                             address indexed oldOracle,
+                             address indexed newOracle);
+
     //--------------------------------------------------------------------------
     // Events
-
-    // @todo Should events be KTT or KOL denominated?
 
     event BackingInBPSChanged(uint oldBackingInBPS, uint newBackingInBPS);
 
     //----------------------------------
     // Owner Events
+
+    event PriceFloorChanged(uint oldPriceFloor, uint newPriceFloor);
+
+    event PriceCeilingChanged(uint oldPriceCeiling, uint newPriceCeiling);
 
     event MinBackingInBPSChanged(uint oldMinBackingInBPS,
                                  uint newMinBackingInBPS);
@@ -58,18 +77,21 @@ contract Reserve is Ownable, Whitelisted {
     /// @dev The min amount in bps of reserve to supply.
     uint private constant MIN_BACKING_IN_BPS = 5_000; // 50%
 
-    // @todo This is wrong! KTT uses 18 decimals!
-    uint private constant KTT_DECIMALS = 9;
+    /// @dev Note that KTT and KOL use the same decimal precision.
+    uint private constant KTT_DECIMALS = 18;
     uint private constant KOL_DECIMALS = 18;
 
     //--------------------------------------------------------------------------
     // Storage
 
-    /// @dev The KOL token address.
+    /// @dev The KOL token implementation.
     KOL private immutable _kol;
 
-    /// @dev The KTT token address.
+    /// @dev The KTT token implementation.
     ERC20 private immutable _ktt;
+
+    /// @dev The cUSD token implementation.
+    ERC20 private immutable _cUSD;
 
     /// @dev The bps of supply backed by the reserve.
     uint private _backingInBPS;
@@ -78,21 +100,58 @@ contract Reserve is Ownable, Whitelisted {
     /// @dev Changeable by owner.
     uint public minBackingInBPS;
 
+    /// @notice The anticipated price ceiling for KOL.
+    /// @dev Denominated in USD with 18 decimal precision.
+    /// @dev Changeable by owner.
+    uint public priceCeiling;
+
+    /// @notice The anticipated price floor for KOL.
+    /// @dev Denominated in USD with 18 decimal precision.
+    /// @dev Changeable by owner.
+    uint public priceFloor;
+
+    /// @notice The cUSD price oracle.
+    /// @dev Denominated in USD with 18 decimal precision.
+    /// @dev Of type IOracle.
+    /// @dev Changeable by owner.
+    address public oracleCUSD;
+
+    /// @notice The KOL price oracle.
+    /// @dev Denominated in USD with 18 decimal precision.
+    /// @dev Of type IOracle.
+    /// @dev Changeable by owner.
+    address public oracleKOL;
+
     //--------------------------------------------------------------------------
     // Constructor
 
     constructor(
         address kol_,
         address ktt_,
-        uint minBackingInBPS_
+        address cUSD_,
+        uint minBackingInBPS_,
+        address oracleKOL_,
+        address oracleCUSD_
     ) {
         require(kol_ != address(0));
         require(ktt_ != address(0));
+        require(cUSD_ != address(0));
         require(minBackingInBPS_ >= MIN_BACKING_IN_BPS);
 
+        // Fail if oracles do not deliver valid data.
+        bool valid;
+        (/*price*/, valid) = _queryOracle(oracleKOL_);
+        require(valid);
+        (/*price*/, valid) = _queryOracle(oracleCUSD_);
+        require(valid);
+
+        // Set storage.
         _kol = KOL(kol_);
         _ktt = ERC20(ktt_);
+        _cUSD = ERC20(cUSD_);
         minBackingInBPS = minBackingInBPS_;
+        oracleKOL = oracleKOL_;
+        oracleCUSD = oracleCUSD_;
 
         // Set current backing to 100%.
         _backingInBPS = BPS;
@@ -101,17 +160,16 @@ contract Reserve is Ownable, Whitelisted {
     //--------------------------------------------------------------------------
     // User Mutating Functions
 
+    // @todo depositAll, depositAllFor etc. (ButtonWrapper).
+
     /// @notice Deposits KTT tokens from msg.sender and mints corresponding KOL
     ///         tokens to msg.sender.
     /// @param ktts The amount of KTT tokens to deposit.
-    function deposit(uint ktts)
-        external
-        onlyWhitelisted
-    {
+    function deposit(uint ktts) external onlyWhitelisted {
         _ktt.safeTransferFrom(msg.sender, address(this), ktts);
 
-        uint kols = kttToKol(ktts);
-        _kol.mint(msg.sender, kols);
+        // Note that the conversion rate of KOL:KTT is 1:1.
+        _kol.mint(msg.sender, ktts);
 
         // @todo Not strictly necessary. But why not use the chance?
         //       Or better create backend task to call function regularly?
@@ -123,23 +181,94 @@ contract Reserve is Ownable, Whitelisted {
     /// @notice Withdraws KTT tokens to msg.sender and burns KOL tokens from
     ///         msg.sender.
     /// @param kols The amount of KOL tokens to burn.
-    function withdraw(uint kols)
-        external
-        onlyWhitelisted
-    {
-        uint ktts = kolToKtt(kols);
-        _ktt.safeTransfer(msg.sender, ktts);
+    function withdraw(uint kols) external onlyWhitelisted {
+        // Note that the conversion rate of KOL:KTT is 1:1.
+        _ktt.safeTransfer(msg.sender, kols);
 
         _kol.burn(msg.sender, kols);
 
         // @todo See function deposit.
         _updateBackingInBPS();
 
-        emit Withdrawal(msg.sender, ktts);
+        emit Withdrawal(msg.sender, kols);
+    }
+
+    function swapExactCUSDForKOL(uint amount) external onlyWhitelisted {
+
+    }
+
+    function swapExactKOLForCUSD(uint amount) external onlyWhitelisted {
+
     }
 
     //--------------------------------------------------------------------------
     // onlyOwner Functions
+
+    //----------------------------------
+    // Oracle Management
+
+    function setCUSDPriceOracle(address oracle) external onlyOwner {
+        // Return early if state does not change.
+        if (oracleCUSD == oracle) {
+            return;
+        }
+
+        // Fail if oracle does not deliver valid data.
+        bool valid;
+        (/*price*/, valid) = _queryOracle(oracle);
+        if (!valid) {
+            revert StalePriceDeliveredByOracle(address(_cUSD), oracle);
+        }
+
+        // Update oracle and emit event.
+        emit AssetOracleUpdated(address(_cUSD), oracleCUSD, oracle);
+        oracleCUSD = oracle;
+    }
+
+    function setKOLPriceOracle(address oracle) external onlyOwner {
+        // Return early if state does not change.
+        if (oracleKOL == oracle) {
+            return;
+        }
+
+        // Fail if oracle does not deliver valid data.
+        bool valid;
+        (/*price*/, valid) = _queryOracle(oracle);
+        if (!valid) {
+            revert StalePriceDeliveredByOracle(address(_kol), oracle);
+        }
+
+        // Update oracle and emit event.
+        emit AssetOracleUpdated(address(_kol), oracleKOL, oracle);
+        oracleKOL = oracle;
+    }
+
+    //----------------------------------
+    // Price Floor/Ceiling Management
+
+    function setPriceFloor(uint priceFloor_) external onlyOwner {
+        require(priceFloor_ <= priceCeiling && priceFloor_ != 0);
+
+        // Return early if state does not change.
+        if (priceFloor == priceFloor_) {
+            return;
+        }
+
+        emit PriceFloorChanged(priceFloor, priceFloor_);
+        priceFloor = priceFloor_;
+    }
+
+    function setPriceCeiling(uint priceCeiling_) external onlyOwner {
+        require(priceCeiling_ >= priceFloor && priceCeiling_ != 0);
+
+        // Return early if state does not change.
+        if (priceCeiling == priceCeiling_) {
+            return;
+        }
+
+        emit PriceCeilingChanged(priceCeiling, priceCeiling_);
+        priceCeiling = priceCeiling_;
+    }
 
     //----------------------------------
     // Debt Management
@@ -147,13 +276,12 @@ contract Reserve is Ownable, Whitelisted {
     function setMinBackingInBPS(uint minBackingInBPS_) external onlyOwner {
         require(minBackingInBPS_ >= MIN_BACKING_IN_BPS);
 
-        // Only emit event is state changed.
+        // Return early if state does not change.
         if (minBackingInBPS == minBackingInBPS_) {
             return;
         }
 
         emit MinBackingInBPSChanged(minBackingInBPS, minBackingInBPS_);
-
         minBackingInBPS = minBackingInBPS_;
     }
 
@@ -166,10 +294,8 @@ contract Reserve is Ownable, Whitelisted {
 
         // @todo Emit event, adjust tests.
 
-        // Note that this function is KTT denominated!
-        uint kols = kttToKol(ktts);
-
-        _kol.mint(msg.sender, kols);
+        // Note that the conversion rate of KOL:KTT is 1:1.
+        _kol.mint(msg.sender, ktts);
 
         _updateBackingInBPS();
 
@@ -249,15 +375,15 @@ contract Reserve is Ownable, Whitelisted {
                 : (reserveAdjusted * BPS) / supply; // Partially backed
 
         emit BackingInBPSChanged(_backingInBPS, newBackingInBPS);
-
         _backingInBPS = newBackingInBPS;
     }
 
     /// @dev Returns the current reserve in USD denomination with 18 decimal
     ///      precision.
     function _reserveAdjusted() private view returns (uint) {
-        // Note to convert reserve (KTT) to 18 decimals.
-        return kttToKol(_ktt.balanceOf(address(this)));
+        // Note that KTT is in 18 decimal precision and is assumed to always
+        // have an intrinsic value of 1 USD.
+        return _ktt.balanceOf(address(this));
     }
 
     /// @dev Returns the current supply in USD denomination with 18 decimal
@@ -266,21 +392,12 @@ contract Reserve is Ownable, Whitelisted {
         return _kol.totalSupply();
     }
 
-    // @todo Propably uneccessary. KTT uses 18 decimals!
+    function _queryOracle(address oracle) private returns (uint, bool) {
+        uint data;
+        bool valid;
+        (data, valid) = IOracle(oracle).getData();
 
-    // Note that KOL and KTT have different precision decimals.
-    function kolToKtt(uint kols) private pure returns (uint) {
-        // @todo Assumes KOL should be 1 USD.
-        // @todo Can lead to zero! Be careful.
-        //       -> Remove comment if tests implemented to specify behaviour.
-        //       -> require(kols >= 1e9)?
-        return kols / 1e9;
-    }
-
-    // Note that KOL and KTT have different precision decimals.
-    function kttToKol(uint ktts) private pure returns (uint) {
-        // @todo Assumes KOL should be 1 USD.
-        return ktts * 1e9;
+        return (data, valid);
     }
 
 }

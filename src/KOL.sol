211f6c90afc41f20fcea3322df0ba6bd7a08c0f4
pascal <pascal@merkleplant.xyz>
2022-04-08 10:28:41 +0200

    KOL: Add whitelist to manage mint/burn allowance. Add ERC20 modifiers

--- src/KOL.sol
+++ src/KOL.sol
@@ -5,87 +5,104 @@ import {ERC20} from "solmate/tokens/ERC20.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 
 import {Ownable} from "solrocket/Ownable.sol";
+import {Whitelisted} from "solrocket/Whitelisted.sol";
 
-contract KOL is ERC20, Ownable {
+/**
+ * @title KOL Token
+ *
+ * @dev The KOL ERC20 token is a non-elastic supply token.
+ *      A whitelist, managed by the contract's owner, manages mint/burn
+ *      permissions for addresses.
+ *
+ *      The whitelist is necessary to allow different reserves to mint/burn
+ *      the KOL token to addresses depositing KTT tokens into that reserve.
+ *
+ * @author byterocket
+ */
+contract KOL is ERC20, Ownable, Whitelisted {
 
     //--------------------------------------------------------------------------
     // Errors
 
-    /// @notice Function is only callable by mintBurner.
-    error OnlyCallableByMintBurner();
+    /// @notice Invalid token recipient.
+    error InvalidRecipient();
 
-    //--------------------------------------------------------------------------
-    // Events
-
-    /// @notice Event emitted when mintBurner changed.
-    event MintBurnerChanged(address oldMintBurner, address newMintBurner);
+    /// @notice Invalid token amount.
+    error InvalidAmount();
 
     //--------------------------------------------------------------------------
     // Modifiers
 
-    /// @notice Modifier to guarantee function is only callable by mintBurner.
-    modifier onlyMintBurner() {
-        if (msg.sender != mintBurner) {
-            revert OnlyCallableByMintBurner();
-        } else {
-            _;
+    /// @dev Modifier to guarantee token recipient is valid.
+    modifier validRecipient(address to) {
+        if (to == address(0) || to == address(this)) {
+            revert InvalidRecipient();
         }
+        _;
     }
 
-    // @todo Create validAmount and validRecipient modifier.
-
-    //--------------------------------------------------------------------------
-    // Storage
-
-    /// @notice The mintBurner address with the allowance to mint and burn KOL
-    ///         tokens.
-    /// @dev Changeable by owner.
-    address public mintBurner;
-
+    /// @dev Modifier to guarantee token amount is valid.
+    modifier validAmount(uint amount) {
+        if (amount == 0) {
+            revert InvalidAmount();
+        }
+        _;
+    }
     //--------------------------------------------------------------------------
     // Constructor
 
-    constructor(address mintBurner_)
-        ERC20("Kolektivo Reserve Token", "KOL", uint8(18))
-    {
-        require(
-            mintBurner_ != address(this) &&
-            mintBurner_ != owner         &&
-            mintBurner_ != address(0)
-        );
-        mintBurner = mintBurner_;
+    constructor() ERC20("Kolektivo Reserve Token", "KOL", uint8(18)) {
+        // NO-OP
     }
 
     //--------------------------------------------------------------------------
-    // onlyMintBurner Mutating Functions
+    // onlyWhitelisted Mutating Functions
 
     /// @notice Mints an amount of KOL tokens to some address.
-    /// @dev Only callable by mintBurner.
-    function mint(address to, uint amount) external onlyMintBurner {
+    /// @dev Only callable by whitelisted address.
+    function mint(address to, uint amount) external onlyWhitelisted {
         super._mint(to, amount);
     }
 
     /// @notice Burns an amount of KOL tokens from some address.
-    /// @dev Only callable by mintBurner.
-    function burn(address from, uint amount) external onlyMintBurner {
+    /// @dev Only callable by whitelisted address.
+    function burn(address from, uint amount) external onlyWhitelisted {
         super._burn(from, amount);
     }
 
     //--------------------------------------------------------------------------
-    // onlyOwner Mutating Functions
+    // Overriden ERC20 Mutating Functions
+    //
+    // Note that the functions are overidden in order to enforce the validAmount
+    // and validRecipient modifiers.
 
-    /// @notice Changes the mintBurner address.
-    /// @dev Only callable by owner.
-    function setMintBurner(address mintBurner_) external onlyOwner {
-        require(
-            mintBurner_ != address(this) &&
-            mintBurner_ != owner         &&
-            mintBurner_ != mintBurner    &&
-            mintBurner_ != address(0)
-        );
+    function approve(address spender, uint amount)
+        public
+        override(ERC20)
+        validRecipient(spender)
+        returns (bool)
+    {
+        return super.approve(spender, amount);
+    }
 
-        emit MintBurnerChanged(mintBurner, mintBurner_);
-        mintBurner = mintBurner_;
+    function transfer(address to, uint amount)
+        public
+        override(ERC20)
+        validRecipient(to)
+        validAmount(amount)
+        returns (bool)
+    {
+        return super.transfer(to, amount);
+    }
+
+    function transferFrom(address from, address to, uint amount)
+        public
+        override(ERC20)
+        validRecipient(to)
+        validAmount(amount)
+        returns (bool)
+    {
+        return super.transferFrom(from, to, amount);
     }
 
 }

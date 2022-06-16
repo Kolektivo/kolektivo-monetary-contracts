fb8cbed006622c66a54c43df4f64e61a7a228da0
pascal <pascal@merkleplant.xyz>
2022-06-16 12:13:58 +0200

    GeoNFT: Refactor tests to single file

--- test/geoNFT/GeoNFT.t.sol
+++ test/geoNFT/GeoNFT.t.sol
@@ -1,17 +1,80 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.10;
 
-import "./Test.t.sol";
+import "forge-std/Test.sol";
+
+import "src/GeoNFT.sol";
+import "src/lib/GeoCoordinates.sol";
 
 /**
- * @dev onlyOwner Function Tests.
+ * Errors library for GeoNFT's custom errors.
+ * Enables checking for errors with vm.expectRevert(Errors.<Error>).
  */
-contract GeoNFTOnlyOwner is GeoNFTTest {
+library Errors {
+    bytes internal constant OnlyCallableByOwner =
+        abi.encodeWithSignature("OnlyCallableByOwner()");
+
+    bytes internal constant InvalidTokenId =
+        abi.encodeWithSignature("GeoNFT__InvalidTokenId()");
+
+    bytes internal constant InvalidRecipient =
+        abi.encodeWithSignature("GeoNFT__InvalidRecipient()");
+
+    bytes internal constant InvalidLatitude =
+        abi.encodeWithSignature("GeoNFT__InvalidLatitude()");
+
+    bytes internal constant InvalidLongitude =
+        abi.encodeWithSignature("GeoNFT__InvalidLongitude()");
+
+    bytes internal constant InvalidIdentifier =
+        abi.encodeWithSignature("GeoNFT__InvalidIdentifier()");
+}
+
+/**
+ * @dev Root contract for GeoNFT Test Contracts.
+ *
+ *      Provides the setUp functions, access to common test utils and internal
+ *      variables used throughout testing.
+ */
+contract GeoNFTTest is Test {
+    // SuT.
+    GeoNFT geoNFT;
+
+    // Event copied from SuT.
+    // Note that the Event declarations are needed to test for emission.
+    event TokenModified(uint indexed id);
+
+    // Contructor arguments.
+    string internal constant name = "GeoNFT";
+    string internal constant symbol = "GNFT";
+
+    // Other constants.
+    uint internal constant FIRST_TOKEN_ID = 1;
+
+    //--------------------------------------------------------------------------
+    // Set Up Functions
+
+    function setUp() public {
+        geoNFT = new GeoNFT(name, symbol);
+    }
+
+    //--------------------------------------------------------------------------
+    // Deployment Tests
+
+    function testDeploymentInvariants() public {
+        assertEq(geoNFT.owner(), address(this));
+    }
+
+    function testDeploymentConstructor() public {
+        assertEq(geoNFT.name(), name);
+        assertEq(geoNFT.symbol(), symbol);
+    }
+
+    //--------------------------------------------------------------------------
+    // onlyOwner Tests
 
     function testOnlyOwnerFunctionsNotPubliclyCallable(address caller) public {
-        if (caller == geoNFT.owner()) {
-            return;
-        }
+        vm.assume(caller != geoNFT.owner());
         vm.startPrank(caller);
 
         vm.expectRevert(Errors.OnlyCallableByOwner);
@@ -24,6 +87,9 @@ contract GeoNFTOnlyOwner is GeoNFTTest {
         geoNFT.burn(0);
     }
 
+    //--------------------------------------------------------------------------
+    // Mint Tests
+
     function testMint(
         address to,
         int32 latitude,
@@ -31,6 +97,7 @@ contract GeoNFTOnlyOwner is GeoNFTTest {
         string memory identifier
     ) public {
         // Expect revert if token recipient is zero address or address(geoNFT).
+        // Modifier: validRecipient.
         if (to == address(0) || to == address(geoNFT)) {
             vm.expectRevert(Errors.InvalidRecipient);
             geoNFT.mint(to, latitude, longitude, identifier);
@@ -38,6 +105,7 @@ contract GeoNFTOnlyOwner is GeoNFTTest {
         }
 
         // Expect revert if latitude coordinate is invalid.
+        // Modifier: validLatitude.
         if (!GeoCoordinates.isValidLatitudeCoordinate(latitude)) {
             vm.expectRevert(Errors.InvalidLatitude);
             geoNFT.mint(to, latitude, longitude, identifier);
@@ -45,6 +113,7 @@ contract GeoNFTOnlyOwner is GeoNFTTest {
         }
 
         // Expect revert if longitude coordinate is invalid.
+        // Modifier: validLongitude.
         if (!GeoCoordinates.isValidLongitudeCoordinate(longitude)) {
             vm.expectRevert(Errors.InvalidLongitude);
             geoNFT.mint(to, latitude, longitude, identifier);
@@ -52,6 +121,7 @@ contract GeoNFTOnlyOwner is GeoNFTTest {
         }
 
         // Expect revert if identifier is invalid.
+        // Modifier: validIdentifier.
         if (bytes(identifier).length == 0) {
             vm.expectRevert(Errors.InvalidIdentifier);
             geoNFT.mint(to, latitude, longitude, identifier);
@@ -86,6 +156,9 @@ contract GeoNFTOnlyOwner is GeoNFTTest {
         assertEq(geoNFT.ownerOf(FIRST_TOKEN_ID + 1), to);
     }
 
+    //--------------------------------------------------------------------------
+    // Modify Tests
+
     function testModify(
         int32 latitude,
         int32 longitude,
@@ -96,6 +169,7 @@ contract GeoNFTOnlyOwner is GeoNFTTest {
         _mintTokenTo(address(this));
 
         // Expect revert if latitude coordinate is invalid.
+        // Modifier: validLatitude.
         if (!GeoCoordinates.isValidLatitudeCoordinate(latitude)) {
             vm.expectRevert(Errors.InvalidLatitude);
             geoNFT.modify(FIRST_TOKEN_ID, latitude, longitude, identifier);
@@ -103,6 +177,7 @@ contract GeoNFTOnlyOwner is GeoNFTTest {
         }
 
         // Expect revert if longitude coordinate is invalid.
+        // Modifier: validLongitude.
         if (!GeoCoordinates.isValidLongitudeCoordinate(longitude)) {
             vm.expectRevert(Errors.InvalidLongitude);
             geoNFT.modify(FIRST_TOKEN_ID, latitude, longitude, identifier);
@@ -110,6 +185,7 @@ contract GeoNFTOnlyOwner is GeoNFTTest {
         }
 
         // Expect revert if identifier is invalid.
+        // Modifier: validIdentifier.
         if (bytes(identifier).length == 0) {
             vm.expectRevert(Errors.InvalidIdentifier);
             geoNFT.modify(FIRST_TOKEN_ID, latitude, longitude, identifier);
@@ -139,6 +215,24 @@ contract GeoNFTOnlyOwner is GeoNFTTest {
         assertEq(gotIdentifier, identifier);
     }
 
+    function testModifyInvalidTokenId(uint tokenId) public {
+        // Don't go to crazy with minting.
+        vm.assume(tokenId > 0 && tokenId < 10);
+
+        // Mint tokenId - 1 NFTs.
+        for (uint i; i < tokenId - 1; i++) {
+            _mintTokenTo(address(1));
+        }
+
+        // Modifying non-existing tokenId should revert.
+        // Modifer: validTokenId.
+        vm.expectRevert(Errors.InvalidTokenId);
+        geoNFT.modify(tokenId, 0, 0, "");
+    }
+
+    //--------------------------------------------------------------------------
+    // Burn Tests
+
     function testBurn() public {
         _mintTokenTo(address(this));
 
@@ -151,6 +245,24 @@ contract GeoNFTOnlyOwner is GeoNFTTest {
         // Check that old owner's balance decreased.
         assertEq(geoNFT.balanceOf(address(this)), 0);
     }
+
+    function testBurnInvalidTokenId(uint tokenId) public {
+        // Don't go to crazy with minting.
+        vm.assume(tokenId > 0 && tokenId < 10);
+
+        // Mint tokenId - 1 NFTs.
+        for (uint i; i < tokenId - 1; i++) {
+            _mintTokenTo(address(1));
+        }
+
+        // Burning non-existing tokenId should revert.
+        // Modifier: validTokenId.
+        vm.expectRevert(Errors.InvalidTokenId);
+        geoNFT.burn(tokenId);
+    }
+
+    //--------------------------------------------------------------------------
+    // Helper
 
     function _mintTokenTo(address to) private {
         geoNFT.mint(to, 0, 0, "identifier");

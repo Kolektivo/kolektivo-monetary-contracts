08a7c67cd46efabf07997b37ba4b7042fd52593c
SchneiderNejc <nejc.sch@gmail.com>
2022-08-22 22:42:57 +0200

    VestingVault: update tests - add testDepositFor

--- test/vesting/VestingVaultNejc.t.sol
+++ test/vesting/VestingVaultNejc.t.sol
@@ -3,7 +3,11 @@ pragma solidity 0.8.10;
 
 import "forge-std/Test.sol";
 
-import "src/VestingVaultNejc.sol";
+import "src/vesting/VestingVaultNejc.sol";
+
+import {ERC20Mock} from "../utils/mocks/ERC20Mock.sol";
+
+
 
 /**
  * Errors library for VestingVaultNejc's custom errors.
@@ -16,245 +20,146 @@ library Errors {
     bytes internal constant InvalidAmount =
         abi.encodeWithSignature("InvalidAmount()");
 
-    bytes internal constant InvalidVestingDuration =
-        abi.encodeWithSignature("InvalidVestingDuration()");
+    bytes internal constant InvalidDuration =
+        abi.encodeWithSignature("InvalidDuration()");
 
     bytes internal constant InvalidVestingsData =
         abi.encodeWithSignature("InvalidVestingsData()");
 }
 
 /**
- * @dev Root contract for VaultVestingNejc Test Contracts.
+ * @dev Root contract for VestingVaultNejc Test Contracts.
  *
  *      Provides the setUp functions, access to common test utils and internal
  *      variables used throughout testing.
  */
-contract VaultVestingNejcTest is Test {
+contract VestingVaultNejcTest is Test {
     // SuT.
-    VaultVestingNejc vaultVestingNejc;
+    ERC20Mock token;
+    VestingVaultNejc vestingVaultNejc;
 
     // Event copied from SuT.
     // Note that the Event declarations are needed to test for emission.
     event DepositFor(address indexed investor, address indexed receiver, uint amount, uint duration);
     event Claim(address indexed receiver, uint withdrawnAmount);
 
-    // Contructor arguments.
-    string internal constant token_= [tokenAddress];
-
-    // Other constants.
-    //uint internal constant FIRST_TOKEN_ID = 1;
+    // Token contructor arguments.
+    string internal constant NAME = "testToken";
+    string internal constant SYMBOL = "TT";
+    uint internal constant DECIMALS = 18;
 
     //--------------------------------------------------------------------------
     // Set Up Functions
 
     function setUp() public {
-        vaultVestingNejc = new VaultVestingNejc(token_);
+        // contracts deployment
+        token = new ERC20Mock(NAME, SYMBOL, uint8(DECIMALS));
+        vestingVaultNejc = new VestingVaultNejc(address(token));
     }
 
     //--------------------------------------------------------------------------
     // Deployment Tests
 
+    // function testDeploymentInvariants() public {
     function testDeploymentConstructor() public {
-        assertEq(geoNFT.name(), name);
-        assertEq(geoNFT.symbol(), symbol);
+        assertEq(token.name(), NAME);
+        assertEq(token.symbol(), SYMBOL);
+        assertEq(token.decimals(), DECIMALS);
+        assertEq(vestingVaultNejc.token(), address(token));
     }
 
     //--------------------------------------------------------------------------
-    // onlyOwner Tests
+    //Deposit Tests
 
-    function testOnlyOwnerFunctionsNotPubliclyCallable(address caller) public {
-        vm.assume(caller != geoNFT.owner());
-        vm.startPrank(caller);
+    function testDepositFor(address receiver, uint amount, uint duration) public {
+        // mint tokens for investor
+        token.mint(address(this), amount);
+        assertEq(token.balanceOf(address(this)), amount);
 
-        vm.expectRevert(Errors.OnlyCallableByOwner);
-        geoNFT.mint(address(1), 0, 0, "identifier");
-
-        vm.expectRevert(Errors.OnlyCallableByOwner);
-        geoNFT.modify(0, 0, 0, "identifier");
-
-        vm.expectRevert(Errors.OnlyCallableByOwner);
-        geoNFT.burn(0);
-    }
-
-    //--------------------------------------------------------------------------
-    // Mint Tests
-
-    function testMint(
-        address to,
-        int32 latitude,
-        int32 longitude,
-        string memory identifier
-    ) public {
-        // Expect revert if token recipient is zero address or address(geoNFT).
+        // console2.log()
+        // Expect revert if receiver is invalid.
         // Modifier: validRecipient.
-        if (to == address(0) || to == address(geoNFT)) {
+        if (receiver == address(0)                  ||
+            receiver == address(vestingVaultNejc)   ||
+            receiver == address(this)               ||
+            receiver == address(token)
+        ) {
             vm.expectRevert(Errors.InvalidRecipient);
-            geoNFT.mint(to, latitude, longitude, identifier);
+            vestingVaultNejc.depositFor(receiver, amount, duration);
+            return;
+        }
+        //
+        // // Expect revert if amount is zero.
+        // // Modifier: validAmount.
+        if (amount == 0 || amount > 10) {
+            vm.expectRevert(Errors.InvalidAmount);
+            vestingVaultNejc.depositFor(receiver, amount, duration);
             return;
         }
 
-        // Expect revert if latitude coordinate is invalid.
-        // Modifier: validLatitude.
-        if (!GeoCoordinates.isValidLatitudeCoordinate(latitude)) {
-            vm.expectRevert(Errors.InvalidLatitude);
-            geoNFT.mint(to, latitude, longitude, identifier);
+        // Expect revert if vestingDuration is zero.
+        // Modifier: validVestingDuration.
+        if (duration == 0 || duration > 10e8) {
+            vm.expectRevert(Errors.InvalidDuration);
+            vestingVaultNejc.depositFor(receiver, amount, duration);
             return;
         }
 
-        // Expect revert if longitude coordinate is invalid.
-        // Modifier: validLongitude.
-        if (!GeoCoordinates.isValidLongitudeCoordinate(longitude)) {
-            vm.expectRevert(Errors.InvalidLongitude);
-            geoNFT.mint(to, latitude, longitude, identifier);
-            return;
-        }
 
-        // Expect revert if identifier is invalid.
-        // Modifier: validIdentifier.
-        if (bytes(identifier).length == 0) {
-            vm.expectRevert(Errors.InvalidIdentifier);
-            geoNFT.mint(to, latitude, longitude, identifier);
-            return;
-        }
+        // Otherwise expect tokens to be deposited.
+        token.approve(address(vestingVaultNejc), amount);
+        assertEq(token.allowance(address(this), address(vestingVaultNejc)), amount);
 
-        // Otherwise expect token to be minted.
-        geoNFT.mint(to, latitude, longitude, identifier);
+        vestingVaultNejc.depositFor(receiver, amount, duration);
+        // TODO check emitted event
 
-        // Check token owner.
-        assertEq(geoNFT.ownerOf(FIRST_TOKEN_ID), to);
-        assertEq(geoNFT.balanceOf(to), 1);
+        // Check vestingVaultNejc balances
+        assertEq(token.balanceOf(address(vestingVaultNejc)), amount);
 
-        // Check token data.
-        uint gotlastModified;
-        int32 gotLatitude;
-        int32 gotLongitude;
-        string memory gotIdentifier;
-        (gotlastModified, gotLatitude, gotLongitude, gotIdentifier)
-            = geoNFT.tokenData(FIRST_TOKEN_ID);
+        uint totalVested = vestingVaultNejc.getTotalVestedFor(receiver);
+        assertEq(totalVested, amount);
 
-        assertEq(gotlastModified, block.timestamp);
-        assertEq(gotLatitude, latitude);
-        assertEq(gotLongitude, longitude);
-        assertEq(gotIdentifier, identifier);
-
-        // Mint another token to verify token id is incremented correctly.
-        // Note that it's fine to have two tokes with the exact same data.
-        geoNFT.mint(to, latitude, longitude, identifier);
-
-        // Only check that token id is incremented correctly.
-        assertEq(geoNFT.ownerOf(FIRST_TOKEN_ID + 1), to);
+        /// TODO:
+        // uint totalClaimable = vestingVaultNejc.getTotalNotClaimableYetFor(receiver);
+        // //      assert totalClaimable < totalVested
+        //
+        // uint totalNotClaimableYetFor = vestingVaultNejc.getTotalNotClaimableYetFor(receiver);
+        //      assert totalClaimable < totalNonClaimable
+        // after vesting is complete, make sure its not possible to claim anymore
     }
+
+
 
     //--------------------------------------------------------------------------
-    // Modify Tests
+    // Claim Tests
 
-    function testModify(
-        int32 latitude,
-        int32 longitude,
-        string memory identifier
-    ) public {
-        // Note that GeoNFT does not use safeMint, i.e. ERC721TokenReceiver is
-        // not checked for contracts.
-        _mintTokenTo(address(this));
-
-        // Expect revert if latitude coordinate is invalid.
-        // Modifier: validLatitude.
-        if (!GeoCoordinates.isValidLatitudeCoordinate(latitude)) {
-            vm.expectRevert(Errors.InvalidLatitude);
-            geoNFT.modify(FIRST_TOKEN_ID, latitude, longitude, identifier);
-            return;
-        }
-
-        // Expect revert if longitude coordinate is invalid.
-        // Modifier: validLongitude.
-        if (!GeoCoordinates.isValidLongitudeCoordinate(longitude)) {
-            vm.expectRevert(Errors.InvalidLongitude);
-            geoNFT.modify(FIRST_TOKEN_ID, latitude, longitude, identifier);
-            return;
-        }
-
-        // Expect revert if identifier is invalid.
-        // Modifier: validIdentifier.
-        if (bytes(identifier).length == 0) {
-            vm.expectRevert(Errors.InvalidIdentifier);
-            geoNFT.modify(FIRST_TOKEN_ID, latitude, longitude, identifier);
-            return;
-        }
-
-        // Otherwise expect token to be modified.
-        vm.expectEmit(true, true, true, true);
-        emit TokenModified(FIRST_TOKEN_ID);
-
-        geoNFT.modify(FIRST_TOKEN_ID, latitude, longitude, identifier);
-
-        // Check that token owner did not change.
-        assertEq(geoNFT.ownerOf(FIRST_TOKEN_ID), address(this));
-
-        // Check token data.
-        uint gotlastModified;
-        int32 gotLatitude;
-        int32 gotLongitude;
-        string memory gotIdentifier;
-        (gotlastModified, gotLatitude, gotLongitude, gotIdentifier)
-            = geoNFT.tokenData(FIRST_TOKEN_ID);
-
-        assertEq(gotlastModified, block.timestamp);
-        assertEq(gotLatitude, latitude);
-        assertEq(gotLongitude, longitude);
-        assertEq(gotIdentifier, identifier);
-    }
-
-    function testModifyInvalidTokenId(uint tokenId) public {
-        // Don't go to crazy with minting.
-        vm.assume(tokenId > 0 && tokenId < 10);
-
-        // Mint tokenId - 1 NFTs.
-        for (uint i; i < tokenId - 1; i++) {
-            _mintTokenTo(address(1));
-        }
-
-        // Modifying non-existing tokenId should revert.
-        // Modifer: validTokenId.
-        vm.expectRevert(Errors.InvalidTokenId);
-        geoNFT.modify(tokenId, 0, 0, "");
-    }
-
-    //--------------------------------------------------------------------------
-    // Burn Tests
-
-    function testBurn() public {
-        _mintTokenTo(address(this));
-
-        geoNFT.burn(FIRST_TOKEN_ID);
-
-        // Check that token is not minted anymore.
-        vm.expectRevert("NOT_MINTED"); // Thrown by ERC721 base contract dependency.
-        geoNFT.ownerOf(FIRST_TOKEN_ID);
-
-        // Check that old owner's balance decreased.
-        assertEq(geoNFT.balanceOf(address(this)), 0);
-    }
-
-    function testBurnInvalidTokenId(uint tokenId) public {
-        // Don't go to crazy with minting.
-        vm.assume(tokenId > 0 && tokenId < 10);
-
-        // Mint tokenId - 1 NFTs.
-        for (uint i; i < tokenId - 1; i++) {
-            _mintTokenTo(address(1));
-        }
-
-        // Burning non-existing tokenId should revert.
-        // Modifier: validTokenId.
-        vm.expectRevert(Errors.InvalidTokenId);
-        geoNFT.burn(tokenId);
-    }
-
-    //--------------------------------------------------------------------------
-    // Helper
-
-    function _mintTokenTo(address to) private {
-        geoNFT.mint(to, 0, 0, "identifier");
-    }
+    // function testClaim() public {
+    //
+    //     if (msg.sender ! == receiver) {
+    //         vm.expectRevert(Errors.InvalidVestingsData);
+    //         vestingVaultNejc.claim();
+    //         return;
+    //     }
+    //
+    //     // Otherwise expect tokens to be claimed.
+    //     vestingVaultNejc.depositFor(receiver, amount, duration);
+    //
+    //     // Check vestingVaultNejc balances
+    //     assertEq(testToken.balanceOf(vestingVaultNejc), amount);
+    //     // TODO check that event is emitted
+    //
+    //     uint totalVested = vestingVaultNejc.getTotalVestedFor(receiver);
+    //     assertEq(totalVested, amount);
+    //
+    //     uint totalClaimable = vestingVaultNejc.getTotalNotClaimableYetFor(receiver);
+    //     //      assert totalClaimable < totalVested
+    //
+    //     uint totalNotClaimableYetFor = vestingVaultNejc.getTotalNotClaimableYetFor(receiver);
+    //     //      assert totalClaimable < totalNonClaimable
+    //
+    //     // TODO after vesting is complete, make sure its not possible to claim anymore
+    //
+    //     // TODO make sure event is emitted
+    // }
 
 }

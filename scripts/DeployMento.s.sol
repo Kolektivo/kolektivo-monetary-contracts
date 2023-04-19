255af4398135d207a6422e9b429ea48051c0cfb6
Marvin Kruse <marvinkruse@users.noreply.github.com>
2023-04-19 18:29:32 +0200

    Remove unused import

--- scripts/DeployMento.s.sol
+++ scripts/DeployMento.s.sol
@@ -13,7 +13,6 @@ import {Registry} from "../src/mento/MentoRegistry.sol";
 import {Freezer} from "../src/mento/lib/Freezer.sol";
 import {FixidityLib} from "../src/mento/lib/FixidityLib.sol";
 import {SortedOracles} from "../src/mento/SortedOracles.sol";
-import {UsingRegistry} from "../src/mento/lib/UsingRegistry.sol";
 
 /**
  * @title Mento Deployment Script

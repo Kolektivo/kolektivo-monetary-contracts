116992bcdba3c0fb539269385c50222bc8bd01af
Zitzak <marvin88gr@gmail.com>
2023-04-25 20:18:36 +0200

    chore: add MentoManagement

--- scripts/tasks/MentoManagement.s.sol
+++ scripts/tasks/MentoManagement.s.sol
@@ -8,3 +8,22 @@ import {Registry} from "../../src/mento/MentoRegistry.sol";
 import {Freezer} from "../../src/mento/lib/Freezer.sol";
 import {FixidityLib} from "../../src/mento/lib/FixidityLib.sol";
 import {SortedOracles} from "../../src/mento/SortedOracles.sol";
+
+contract AddOracle is Script {
+    function run() external {
+        // Address allowed to provide price data
+        address oracle = vm.envAddress("TASK_SORTED_ORACLE_PROVIDER");
+        address token = vm.envAddress("TASK_SORTED_ORACLE_TOKEN");
+
+        SortedOracles sortedOracles = SortedOracles(vm.envAddress("DEPLOYMENT_MENTO_SORTED_ORACLES"));
+
+        vm.startBroadcast();
+        {
+            sortedOracles.addOracle(token, oracle);
+        }
+        vm.stopBroadcast();
+
+        console2.log("DataProvider: ", oracle, " set for token: ", token);
+        console2.log("for SortedOracle with address: ", address(sortedOracles));
+    }
+}
